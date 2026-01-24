a0a8df82d0851f224c9a68b202f9da5fae15638d
Tamanna-salma <tamannaummey26@gmail.com>
2026-01-24 11:40:06 +0600

    use state change

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -8,10 +8,9 @@ import toast from 'react-hot-toast';
 
 const LoginPage = () => {
     const [showPassword, setShowPassword] = useState(false);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const router = useRouter();
-
+    const [email, setEmail] = useState('admin@lumensoft.com');
+    const [password, setPassword] = useState('password123');
+    const router = useRouter()
     // Demo Credentials
     const DEMO_EMAIL = "admin@lumensoft.com";
     const DEMO_PASSWORD = "password123";
